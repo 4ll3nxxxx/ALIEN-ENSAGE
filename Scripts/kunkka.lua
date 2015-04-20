89c9205fc10cb8f2789125c35d8ee9ddf33f089a
K L A U S N O M I <theklausnomi@gmail.com>
2015-04-21 06:30:36 +0800

    play

--- Scripts/kunkka.lua
+++ Scripts/kunkka.lua
@@ -1,5 +1,3 @@
---<<Download texture https://mega.co.nz/#!8AZiFAbY!kMdEz0Fezz6cRzpVPrsNJTuUd4RFwHqDSI3cOV51U34 and unpack to nyanui/other>>
-
 require("libs.ScriptConfig")
 require("libs.Skillshot")
 require("libs.Utils")
@@ -9,7 +7,7 @@ config:SetParameter("HotKey", "32", config.TYPE_HOTKEY)
 config:SetParameter("HomeKey", "D", config.TYPE_HOTKEY)
 config:Load()
 
-local play = false local myhero = nil local victim = nil local move = 0 local home = 0
+local play = false local myhero = nil local victim = nil local move = 0 local sleep = {0,0,0,0}
 local rate = client.screenSize.x/1600 local rec = {}
 rec[1] = drawMgr:CreateRect(70*rate,26*rate,270*rate,60*rate,0xFFFFFF30,drawMgr:GetTextureId("NyanUI/other/CM_status_1")) rec[1].visible = false
 rec[2] = drawMgr:CreateText(175*rate,52*rate,0xFFFFFF90,"Target :",drawMgr:CreateFont("manabarsFont","Arial",18*rate,700)) rec[2].visible = false
@@ -28,27 +26,26 @@ function Tick(tick)
 	else
 		foun = Vector(7200,6624,256)
 	end
-	if E.name == "kunkka_x_marks_the_spot" and Q and Q:CanBeCasted() and E.level > 0 and E.abilityPhase then
-		me:CastAbility(Q,victim.position)
-		Sleep(250+client.latency, "combo")
-	end
-	if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
-		me:CastAbility(E)
-		Sleep(250+client.latency, "combo")
+	if tick > sleep[1] then
+		if E.name == "kunkka_x_marks_the_spot" and Q and Q:CanBeCasted() and E.level > 0 and E.abilityPhase then
+			me:CastAbility(Q,victim.position)
+		end
+		if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
+			me:CastAbility(E)
+		end
+		sleep[1] = tick + 150
 	end
 	if IsKeyDown(config.HomeKey) and not client.chat then
 		local travel = me:FindItem("item_tpscroll") or me:FindItem("item_travel_boots")
-		if tick > home and SleepCheck("home") then
+		if tick > sleep[2] then
 			if E and E:CanBeCasted() then
 				me:CastAbility(E,me)
-				Sleep(client.latency,"home")
 			end
 			if travel and travel:CanBeCasted() then
 				me:CastAbility(travel,foun)
-				Sleep(client.latency,"home")
 			end
 		end
-		home = tick + 560
+		sleep[2] = tick + 150
 	end
 	if IsKeyDown(config.HotKey) and not client.chat then
 		rec[1].w = 90*rate + 30*0*rate + 65*rate
@@ -59,32 +56,29 @@ function Tick(tick)
 			rec[z].visible = true
 		end
 		if victim and victim.alive and GetDistance2D(me,victim) <= 2000 then
-			if tick > move then
+			if tick > sleep[3] then
 				if GetDistance2D(victim,me) <= 550 then
 					me:Attack(victim)
 				else
 					me:Follow(victim)
 				end
 			end
-			move = tick + 250
-			if victim and SleepCheck("Zzz") then
+			sleep[3] = tick + 250
+			if tick > sleep[4] then
 				if E.name == "kunkka_x_marks_the_spot" and E:CanBeCasted() and me:CanCast() then
 					me:CastAbility(E,victim)
 					lastpos = victim.position
-					Sleep(client.latency,"Zzz")
 				end
 				if Q and Q:CanBeCasted() and me:CanCast() and E.level > 0 and E.abilityPhase then
 					me:CastAbility(Q,lastpos)
-					Sleep(client.latency,"Zzz")
 				end
 				if R and R:CanBeCasted() and me:CanCast() and Q.level > 0 and Q.abilityPhase then
 					me:CastAbility(R,lastpos)
-					Sleep(client.latency,"Zzz")
 				end
 				if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
 					me:CastAbility(E)
-					Sleep(client.latency,"Zzz")
 				end
+				sleep[4] = tick + 350
 			end
 		end
 	end
