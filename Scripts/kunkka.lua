6665bc76f7a2289c3b5f3e414c4202a75d56503d
K L A U S N O M I <theklausnomi@gmail.com>
2015-04-21 09:12:13 +0800

    play

--- Scripts/kunkka.lua
+++ Scripts/kunkka.lua
@@ -1,5 +1,6 @@
+--<<Download texture https://mega.co.nz/#!8AZiFAbY!kMdEz0Fezz6cRzpVPrsNJTuUd4RFwHqDSI3cOV51U34 and unpack to nyanui/other>>
+
 require("libs.ScriptConfig")
-require("libs.Skillshot")
 require("libs.Utils")
 
 local config = ScriptConfig.new()
@@ -7,7 +8,7 @@ config:SetParameter("HotKey", "32", config.TYPE_HOTKEY)
 config:SetParameter("HomeKey", "D", config.TYPE_HOTKEY)
 config:Load()
 
-local play = false local myhero = nil local victim = nil local move = 0 local sleep = {0,0,0,0}
+local play = false local myhero = nil local victim = nil
 local rate = client.screenSize.x/1600 local rec = {}
 rec[1] = drawMgr:CreateRect(70*rate,26*rate,270*rate,60*rate,0xFFFFFF30,drawMgr:GetTextureId("NyanUI/other/CM_status_1")) rec[1].visible = false
 rec[2] = drawMgr:CreateText(175*rate,52*rate,0xFFFFFF90,"Target :",drawMgr:CreateFont("manabarsFont","Arial",18*rate,700)) rec[2].visible = false
@@ -20,67 +21,65 @@ function Tick(tick)
 	local Q = me:GetAbility(1)
 	local E = me:GetAbility(3)
 	local R = me:GetAbility(4)
-	local victim = FindTarget(me.team)
+
 	if me.team == LuaEntity.TEAM_RADIANT then
 		foun = Vector(-7272,-6757,270)
 	else
 		foun = Vector(7200,6624,256)
 	end
-	if tick > sleep[1] then
+
+	if IsKeyDown(config.HomeKey) and not client.chat then
+		local travel = me:FindItem("item_tpscroll") or me:FindItem("item_travel_boots")
+		if E and E:CanBeCasted() and SleepCheck("home") then
+			me:CastAbility(E,me)
+			Sleep(250+client.latency, "home")
+		end
+		if travel and travel:CanBeCasted() then
+			me:CastAbility(travel,foun)
+			Sleep(250+client.latency, "home")
+		end
+	end
+
+	local victim = FindTarget(me.team)
+	
+	if victim and SleepCheck("combo") then
 		if E.name == "kunkka_x_marks_the_spot" and Q and Q:CanBeCasted() and E.level > 0 and E.abilityPhase then
 			me:CastAbility(Q,victim.position)
+			Sleep(250+client.latency, "combo")
 		end
 		if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
 			me:CastAbility(E)
+			Sleep(250+client.latency, "combo")
 		end
-		sleep[1] = tick + 150
-	end
-	if IsKeyDown(config.HomeKey) and not client.chat then
-		local travel = me:FindItem("item_tpscroll") or me:FindItem("item_travel_boots")
-		if tick > sleep[2] then
-			if E and E:CanBeCasted() then
-				me:CastAbility(E,me)
+
+		if IsKeyDown(config.HotKey) and not client.chat then
+			rec[1].w = 90*rate + 30*0*rate + 65*rate
+			rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate
+			rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate
+			rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
+			for z = 1,3 do
+				rec[z].visible = true
 			end
-			if travel and travel:CanBeCasted() then
-				me:CastAbility(travel,foun)
+
+			if E.name == "kunkka_x_marks_the_spot" and E:CanBeCasted() and me:CanCast() then
+				me:CastAbility(E,victim)
+				lastpos = victim.position
+				Sleep(250+client.latency, "combo")
 			end
-		end
-		sleep[2] = tick + 150
-	end
-	if IsKeyDown(config.HotKey) and not client.chat then
-		rec[1].w = 90*rate + 30*0*rate + 65*rate
-		rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate
-		rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate
-		rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
-		for z = 1,3 do
-			rec[z].visible = true
-		end
-		if victim and victim.alive and GetDistance2D(me,victim) <= 2000 then
-			if tick > sleep[3] then
-				if GetDistance2D(victim,me) <= 550 then
-					me:Attack(victim)
-				else
-					me:Follow(victim)
-				end
+			if R and R:CanBeCasted() and me:CanCast() and E.level > 0 and E.abilityPhase then
+				me:CastAbility(R,lastpos)
+				Sleep(250+client.latency, "combo")
 			end
-			sleep[3] = tick + 250
-			if tick > sleep[4] then
-				if E.name == "kunkka_x_marks_the_spot" and E:CanBeCasted() and me:CanCast() then
-					me:CastAbility(E,victim)
-					lastpos = victim.position
-				end
-				if Q and Q:CanBeCasted() and me:CanCast() and E.level > 0 and E.abilityPhase then
-					me:CastAbility(Q,lastpos)
-				end
-				if R and R:CanBeCasted() and me:CanCast() and Q.level > 0 and Q.abilityPhase then
-					me:CastAbility(R,lastpos)
-				end
-				if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
-					me:CastAbility(E)
-				end
-				sleep[4] = tick + 350
+			if Q and Q:CanBeCasted() and me:CanCast() and R.level > 0 and R.abilityPhase then
+				me:CastAbility(Q,lastpos)
+				Sleep(250+client.latency, "combo")
+			end
+			if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
+				me:CastAbility(E)
+				Sleep(250+client.latency, "combo")
 			end
 		end
+		
 	end
 end
 
@@ -114,9 +113,7 @@ end
 function Close()
 	myhero = nil
 	victim = nil
-	for i = 1, #rec do
-		rec[i].visible = false
-	end
+	start = false
 	collectgarbage("collect")
 	if play then
 		script:UnregisterEvent(Tick)
