6a8a88b005b443a59a319a3cd30adf8c312de473
K L A U S N O M I <theklausnomi@gmail.com>
2015-04-23 11:11:47 +0800

    play

--- Scripts/kunkka.lua
+++ Scripts/kunkka.lua
@@ -8,16 +8,17 @@ config:SetParameter("HotKey", "32", config.TYPE_HOTKEY)
 config:SetParameter("HomeKey", "D", config.TYPE_HOTKEY)
 config:Load()
 
-local play = false local myhero = nil local victim = nil
+local play = false local myhero = nil local victim = nil local sleep = {0,0}
 local rate = client.screenSize.x/1600 local rec = {}
 rec[1] = drawMgr:CreateRect(70*rate,26*rate,270*rate,60*rate,0xFFFFFF30,drawMgr:GetTextureId("NyanUI/other/CM_status_1")) rec[1].visible = false
 rec[2] = drawMgr:CreateText(175*rate,52*rate,0xFFFFFF90,"Target :",drawMgr:CreateFont("manabarsFont","Arial",18*rate,700)) rec[2].visible = false
 rec[3] = drawMgr:CreateRect(220*rate,54*rate,16*rate,16*rate,0xFFFFFF30) rec[3].visible = false
 
-function Tick(tick)
+function Main(tick)
 	if not PlayingGame() then return end
 	local me = entityList:GetMyHero()
 	local ID = me.classId if ID ~= myhero then return end
+	
 	local Q = me:GetAbility(1)
 	local E = me:GetAbility(3)
 	local R = me:GetAbility(4)
@@ -29,64 +30,59 @@ function Tick(tick)
 	end
 
 	if IsKeyDown(config.HomeKey) and not client.chat then
-		local travel = me:FindItem("item_tpscroll") or me:FindItem("item_travel_boots")
-		if E and E:CanBeCasted() and SleepCheck("home") then
-			me:CastAbility(E,me)
-			Sleep(250+client.latency, "home")
-		end
-		if travel and travel:CanBeCasted() then
-			me:CastAbility(travel,foun)
-			Sleep(250+client.latency, "home")
+		if tick > sleep[1] and me.alive then
+			local travel = me:FindItem("item_tpscroll") or me:FindItem("item_travel_boots")
+			if E and E:CanBeCasted() and travel and travel:CanBeCasted() then
+				me:CastAbility(E,me)
+			end
+			if travel and travel:CanBeCasted() then
+				me:CastAbility(travel,foun)
+				sleep[1] = tick + 1000
+			end
 		end
 	end
 
 	local victim = FindTarget(me.team)
+
+	if victim and victim.visible then
+		if not rec[i] then
+			rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
+		end
+	else
+		rec[3].textureId = drawMgr:GetTextureId("NyanUI/spellicons/doom_bringer_empty1")
+	end
 	
-	if victim and SleepCheck("combo") then
+	if victim and tick > sleep[2] then
 		if E.name == "kunkka_x_marks_the_spot" and Q and Q:CanBeCasted() and E.level > 0 and E.abilityPhase then
 			me:CastAbility(Q,victim.position)
-			Sleep(250+client.latency, "combo")
 		end
 		if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
 			me:CastAbility(E)
-			Sleep(250+client.latency, "combo")
 		end
 
 		if IsKeyDown(config.HotKey) and not client.chat then
-			rec[1].w = 90*rate + 30*0*rate + 65*rate
-			rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate
-			rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate
-			rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
-			for z = 1,3 do
-				rec[z].visible = true
-			end
-
 			if E.name == "kunkka_x_marks_the_spot" and E:CanBeCasted() and me:CanCast() then
 				me:CastAbility(E,victim)
 				lastpos = victim.position
-				Sleep(250+client.latency, "combo")
 			end
 			if R and R:CanBeCasted() and me:CanCast() and E.level > 0 and E.abilityPhase then
 				me:CastAbility(R,lastpos)
-				Sleep(250+client.latency, "combo")
 			end
 			if Q and Q:CanBeCasted() and me:CanCast() and R.level > 0 and R.abilityPhase then
 				me:CastAbility(Q,lastpos)
-				Sleep(250+client.latency, "combo")
 			end
 			if E.name == "kunkka_return" and me:CanCast() and math.floor(Q.cd*10) == 110 + math.floor((client.latency/1100)) then
 				me:CastAbility(E)
-				Sleep(250+client.latency, "combo")
 			end
 		end
-		
+		sleep[2] = tick + 100
 	end
 end
 
 function FindTarget(teams)
 	local enemy = entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_HERO and v.team ~= teams and v.visible and v.alive and not v.illusion end)
 	if #enemy == 0 then
-		return entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_HERO and v.team ~= teams end)[1]
+		return entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_HERO and v.team ~= teams and v.visible and v.alive and not v.illusion end)[1]
 	elseif #enemy == 1 then
 		return enemy[1]	
 	else
@@ -104,12 +100,14 @@ function Load()
 		else
 			play = true
 			myhero = me.classId
-			script:RegisterEvent(EVENT_TICK,Tick)
+			rec[1].w = 90*rate + 30*0*rate + 65*rate rec[1].visible = true
+			rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate rec[2].visible = true
+			rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate rec[3].visible = true
+			script:RegisterEvent(EVENT_FRAME, Main)
 			script:UnregisterEvent(Load)
 		end
-	end
+	end	
 end
-
 function Close()
 	myhero = nil
 	victim = nil
