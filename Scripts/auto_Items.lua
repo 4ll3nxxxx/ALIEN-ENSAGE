c5fafb47b10c8f69ecede9cd7a975ec3bb1ea438
K L A U S N O M I <theklausnomi@gmail.com>
2015-05-09 18:01:40 +0800

    play

--- Scripts/auto_Items.lua
+++ Scripts/auto_Items.lua
@@ -12,34 +12,34 @@ function Tick(tick)
 	local phaseboots = me:FindItem("item_phase_boots")
 	local midas = me:FindItem("item_hand_of_midas")
 	local stick = me:FindItem("item_magic_stick") or me:FindItem("item_magic_wand")
+	local shadowplay = me:DoesHaveModifier("modifier_item_silver_edge_windwalk") or me:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") or me:DoesHaveModifier("modifier_clinkz_wind_walk") or me:DoesHaveModifier("modifier_riki_permanent_invisibility") or me:DoesHaveModifier("modifier_bounty_hunter_wind_walk") or me:DoesHaveModifier("modifier_invoker_ghost_walk_self") or me:DoesHaveModifier("modifier_templar_assassin_meld") or me:DoesHaveModifier("modifier_mirana_moonlight_shadow") or me:DoesHaveModifier("modifier_rune_invis")
 
-	if me.alive and not me:IsInvisible() and not me:IsChanneling() then
-		local creeps = entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_CREEP and v.team ~= me.team and v.alive and v.visible and v.spawned and v.level >= 5 and not v.ancient and v.health > 0 and v.attackRange < 650 and v:GetDistance2D(me) < midas.castRange + 25 end)
+	if me.alive and not shadowplay and not me:IsChanneling() then
 		if tick > sleep[1] and midas and midas:CanBeCasted() then
-			for _,v in ipairs(creeps) do
+			for _,v in ipairs(entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_CREEP and v.team ~= me.team and v.alive and v.visible and v.spawned and v.level >= 5 and not v.ancient and v.health > 0 and v.attackRange < 650 and v:GetDistance2D(me) < midas.castRange + 25 end)) do
 				me:CastAbility(midas,v)
-				sleep[1] = tick + 1000 + client.latency
+				sleep[1] = tick + 1000
 			end
 		end
 
 		if tick > sleep[2] and bloodstone and bloodstone:CanBeCasted() and me.health/me.maxHealth < 0.1 then
 			me:CastAbility(bloodstone,me.position)
-			sleep[2] = tick + 1000 + client.latency
+			sleep[2] = tick + 1000
 		end
 
 		if tick > sleep[3] and bottle and bottle:CanBeCasted() and me:DoesHaveModifier("modifier_fountain_aura_buff") and not me:DoesHaveModifier("modifier_bottle_regeneration") and (me.health < me.maxHealth or me.mana < me.maxMana) then
 			me:CastAbility(bottle)
-			sleep[3] = tick + 1000 + client.latency
+			sleep[3] = tick + 1000
 		end
 
 		if tick > sleep[4] and phaseboots and phaseboots:CanBeCasted() then
 			me:CastAbility(phaseboots)
-			sleep[4] = tick + 1000 + client.latency
+			sleep[4] = tick + 1000
 		end
 
 		if tick > sleep[5] and stick and stick:CanBeCasted() and stick.charges > 0 and me.health/me.maxHealth < 0.3 then
 			me:CastAbility(stick)
-			sleep[5] = tick + 1000 + client.latency
+			sleep[5] = tick + 1000
 		end
 
 	end
