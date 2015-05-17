2ccf05679d2c5e1ffdedeb47930b1143a5210877
K L A U S N O M I <theklausnomi@gmail.com>
2015-05-18 03:44:19 +0800

    play

--- Scripts/bountyhunter.lua
+++ Scripts/bountyhunter.lua
@@ -1,54 +1,38 @@
 require("libs.Utils")
 
-local play = false local myhero = nil local dmg = {100,200,250,325}
+local play = false local myhero = nil local dmg = {100,400,250,325}
 
 function Tick(tick)
     if not PlayingGame() then return end
     local me = entityList:GetMyHero()
     local ID = me.classId if ID ~= myhero then return end
-    local Q = me:GetAbility(1) local R = me:GetAbility(4)
-    local heros = entityList:GetEntities({type=LuaEntity.TYPE_HERO,visible=true,alive=true,team=me:GetEnemyTeam(),illusion=false})
-    for i,v in ipairs(heros) do
+    local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,visible=true,alive=true,team=me:GetEnemyTeam(),illusion=false})
+    for i,v in ipairs(enemies) do
+	    local spell = me:GetAbility(1)
+	    local spell4 = me:GetAbility(4)
+    	local distance = GetDistance2D(me,v)
+		local buff = v:DoesHaveModifier("modifier_bounty_hunter_track") or me:DoesHaveModifier("modifier_bounty_hunter_wind_walk")
+		local invis = v:FindItem("item_invis_sword") or v:FindItem("item_shadow_amulet") local irune = v:FindItem("item_bottle")
 
-	    	local distance = GetDistance2D(me,v)
-			local buff = v:DoesHaveModifier("modifier_bounty_hunter_track") or me:DoesHaveModifier("modifier_bounty_hunter_wind_walk")
-			local invis = v:FindItem("item_invis_sword") or v:FindItem("item_shadow_amulet") local irune = v:FindItem("item_bottle")
+		if me.alive and spell and spell:CanBeCasted() and (v.health > 0 and v.health < dmg[spell.level]) and GetDistance2D(v,me) <= spell.castRange and  SleepCheck("spell") then
+			me:SafeCastAbility(spell,v)
+			Sleep(400, "spell")
+		end
 
-		if me.alive then
-
-			if Q and Q:CanBeCasted() and (v.health > 0 and v.health < dmg[Q.level]) and GetDistance2D(v,me) <= Q.castRange and  SleepCheck("q") then
-				me:CastAbility(Q,v)
-				Sleep(400+client.latency,"q")
-			end
-
-			if R and R:CanBeCasted() and not buff and distance <= R.castRange and SleepCheck("r") then
-				if irune and irune.storedRune == 3 then
-					me:CastAbility(R,v)
-				elseif invis and v:CanCast() then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_riki" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_clinkz" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_nyx_assassin" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_templar_assassin" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_broodmother" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_weaver" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_treant" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_sand_king" then
-					me:CastAbility(R,v)
-				elseif v.name == "npc_dota_hero_invoker" then
-					me:CastAbility(R,v)
-				elseif v.health/v.maxHealth < 0.4 then
-					me:CastAbility(R,v)
-					return
-				end
-				Sleep(400+client.latency,"r")
+		if me.alive and spell4 and spell4:CanBeCasted() and not buff and distance <= spell4.castRange and SleepCheck("spell4") then
+			if v.name == "npc_dota_hero_riki" or v.name == "npc_dota_hero_clinkz" or v.name == "npc_dota_hero_nyx_assassin" or v.name == "npc_dota_hero_templar_assassin" or v.name == "npc_dota_hero_broodmother" or v.name == "npc_dota_hero_weaver" or v.name == "npc_dota_hero_treant" or v.name == "npc_dota_hero_sand_king" or v.name == "npc_dota_hero_invoker" then
+				me:SafeCastAbility(spell4,v)
+				Sleep(400, "spell4")
+			elseif irune and irune.storedRune == 3 then
+				me:SafeCastAbility(spell4,v)
+				Sleep(400, "spell4")
+			elseif invis and v:CanCast() then
+				me:SafeCastAbility(spell4,v)
+				Sleep(400, "spell4")
+			elseif v.health/v.maxHealth < 0.4 then
+				me:SafeCastAbility(spell4,v)
+				Sleep(400, "spell4")
+				return
 			end
 		end
 	end
