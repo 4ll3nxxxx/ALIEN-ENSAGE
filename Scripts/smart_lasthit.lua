bf51d45169ce5967e1d8107ac114c485b4ed6459
K L A U S N O M I <theklausnomi@gmail.com>
2015-05-29 00:10:00 +0800

    play

--- Scripts/smart_lasthit.lua
+++ Scripts/smart_lasthit.lua
@@ -2,44 +2,56 @@
 
 require("libs.ScriptConfig")
 require("libs.Utils")
-require("libs.VectorOp")
 
 local config = ScriptConfig.new()
 config:SetParameter("Lasthit", "9", config.TYPE_HOTKEY)
 config:Load()
 
-local play = false local dmg = 0
+local play = false local creeps = nil local dmg = 0
 
 function Main(tick)
 	if not SleepCheck() or not PlayingGame() then return end
 	Sleep(100)
 	local me = entityList:GetMyHero()
+	local creeps = FindCreeps(me)
 	if IsKeyDown(config.Lasthit) and not client.chat then
-		for i,v in ipairs(entityList:FindEntities(function (v) return (v.type == LuaEntity.TYPE_CREEP or v.classId == CDOTA_BaseNPC_Tower) and v.alive and v.visible and GetDistance2D(v,me) <= 2000 end)) do
-			if v.health < v:DamageTaken(dmg + 50 + me.dmgMin + me.dmgBonus, DAMAGE_PHYS, me) then
-				me:Attack(v)
-				if v.health < v:DamageTaken(me.dmgMin + me.dmgBonus, DAMAGE_PHYS, me) then
-					me:Attack(v)
-				else
-					if SleepCheck("attack") then
-						me:Stop()
-						Sleep(250, "attack")
-					end
+		if creeps.health < creeps:DamageTaken(dmg + 50 + me.dmgMin + me.dmgBonus, DAMAGE_PHYS, me) then
+			me:Attack(creeps)
+			if creeps.health < creeps:DamageTaken(me.dmgMin + me.dmgBonus, DAMAGE_PHYS, me) then
+				me:Attack(creeps)
+			else
+				if SleepCheck("stop") then
+					me:Stop()
+					Sleep(250, "stop")
 				end
 			end
 		end
 	end
 end
 
+function FindCreeps(me)
+	local creeps = entityList:GetEntities(function (v) return (v.classId == CDOTA_BaseNPC_Creep_Lane or v.classId == CDOTA_BaseNPC_Creep_Siege or v.classId == CDOTA_BaseNPC_Tower) and (v.team ~= me.team or v.team == me.team) and v.visible and v.alive and v.health > 0 and me:GetDistance2D(v) <= 2000 end)
+	if #creeps == 0 then
+		return entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_HERO and v.team ~= me.team end)[1]
+	elseif #creeps == 1 then
+		return creeps[1]	
+	else
+		table.sort(creeps, function (a,b) return a.health < b.health end)
+		return creeps[1]
+	end
+end
+
 function Load()
 	if PlayingGame() then
 		play = true
-		script:RegisterEvent(EVENT_TICK, Main)
+		creeps = nil
+		script:RegisterEvent(EVENT_FRAME, Main)
 		script:UnregisterEvent(Load)
 	end
 end
 
 function Close()
+	creeps = nil
 	collectgarbage("collect")
 	if play then
 		script:UnregisterEvent(Main)
