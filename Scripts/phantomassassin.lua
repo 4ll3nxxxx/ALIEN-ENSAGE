a899c433c0ac7def0489af5094905fa62e22575e
K L A U S N O M I <theklausnomi@gmail.com>
2015-06-25 02:45:55 +0800

    gggggg

--- Scripts/phantomassassin.lua
+++ Scripts/phantomassassin.lua
@@ -1,36 +1,25 @@
---<<Download texture https://mega.co.nz/#!8AZiFAbY!kMdEz0Fezz6cRzpVPrsNJTuUd4RFwHqDSI3cOV51U34 and unpack to nyanui/other>>
-
-require("libs.ScriptConfig")
+require("libs.HotkeyConfig2")
 require("libs.Utils")
 require("libs.TargetFind")
 require("libs.Animations")
-require("libs.Skillshot")
 
-local config = ScriptConfig.new()
-config:SetParameter("Hotkey", "32", config.TYPE_HOTKEY)
-config:SetParameter("lasthit", "D", config.TYPE_HOTKEY)
-config:Load()
+ScriptConfig = ConfigGUI:New(script.name)
+script:RegisterEvent(EVENT_KEY, ScriptConfig.Key, ScriptConfig)
+script:RegisterEvent(EVENT_TICK, ScriptConfig.Refresh, ScriptConfig)
+ScriptConfig:SetName("Phantom Assassin")
+ScriptConfig:SetExtention(-.3)
+ScriptConfig:SetVisible(false)
 
-local play = false local myhero = nil local victim = nil local start = false local resettime = nil local sleep = {0,0,0} 
-local rate = client.screenSize.x/1600 local rec = {} local castQueue = {}
-rec[1] = drawMgr:CreateRect(70*rate,26*rate,270*rate,60*rate,0xFFFFFF30,drawMgr:GetTextureId("NyanUI/other/CM_status_1")) rec[1].visible = false
-rec[2] = drawMgr:CreateText(175*rate,52*rate,0xFFFFFF90,"Target :",drawMgr:CreateFont("manabarsFont","Arial",18*rate,700)) rec[2].visible = false
-rec[3] = drawMgr:CreateRect(220*rate,54*rate,16*rate,16*rate,0xFFFFFF30) rec[3].visible = false
+ScriptConfig:AddParam("hotkey","Key",SGC_TYPE_ONKEYDOWN,false,false,32)
+ScriptConfig:AddParam("lasthit","Dagger LastHit",SGC_TYPE_ONKEYDOWN,false,false,68)
+ScriptConfig:AddParam("bkb","Auto Black KingBar",SGC_TYPE_TOGGLE,false,true,nil)
+
+play, myhero, victim, start, resettime, castQueue, castsleep, move = false, nil, nil, false, false, {}, 0, 0
 
 function Main(tick)
 	if not PlayingGame() then return end
 	local me = entityList:GetMyHero()
 	local ID = me.classId if ID ~= myhero then return end
-
-	local attackRange = me.attackRange	
-
-	if victim and victim.visible then
-		if not rec[i] then
-			rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
-		end
-	else
-		rec[3].textureId = drawMgr:GetTextureId("NyanUI/spellicons/doom_bringer_empty1")
-	end
 	
 	for i=1,#castQueue,1 do
 		local v = castQueue[1]
@@ -40,31 +29,31 @@ function Main(tick)
 			ability = me:FindItem(ability)
 		end
 		if ability and me:SafeCastAbility(ability,v[3],false) then
-			sleep[3] = tick + v[1]
+			castsleep = tick + v[1] + client.latency
 			return
 		end
 	end
 
-	if IsKeyDown(config.lasthit) and not client.chat then
+	if ScriptConfig.lasthit then
 		local Q = me:GetAbility(1)
 		local creeps = entityList:FindEntities({classId=CDOTA_BaseNPC_Creep_Lane,team=TEAM_ENEMY,alive=true,visible=true,team = me:GetEnemyTeam()})
 		for i,v in ipairs(creeps) do
 			dmg = {60,100,140,180}
-			if Q and Q:CanBeCasted() and v.health < v:DamageTaken(dmg[Q.level],DAMAGE_PURE,me) and GetDistance2D(v,me) <= Q.castRange then
-				table.insert(castQueue,{math.ceil(Q:FindCastPoint()*1000),Q,v})
+			if Q and Q:CanBeCasted() and v.health < v:DamageTaken(dmg[Q.level],DAMAGE_PURE,me) and GetDistance2D(v,me) <= Q.castRange and SleepCheck("cd") then
+				table.insert(castQueue,{math.ceil(Q:FindCastPoint()*1000),Q,v}) Sleep(250 + client.latency, "cd")
 			end
 		end
 	end
 
-	if IsKeyDown(config.Hotkey) and not client.chat then	
-		if Animations.CanMove(me) or not start or (victim and GetDistance2D(victim,me) > attackRange+50) then
+	if ScriptConfig.hotkey then
+		if Animations.CanMove(me) or not start or (victim and GetDistance2D(victim,me) > me.attackRange+50) then
 			start = true
 			local lowestHP = targetFind:GetLowestEHP(3000, phys)
 			if lowestHP and (not victim or victim.creep or GetDistance2D(me,victim) > 600 or not victim.alive or lowestHP.health < victim.health) and SleepCheck("victim") then			
 				victim = lowestHP
 				Sleep(250,"victim")
 			end
-			if victim and GetDistance2D(victim,me) > attackRange+200 and victim.visible then
+			if victim and GetDistance2D(victim,me) > me.attackRange+200 and victim.visible then
 				local closest = targetFind:GetClosestToMouse(me,2000)
 				if closest and (not victim or closest.handle ~= victim.handle) then 
 					victim = closest
@@ -72,54 +61,49 @@ function Main(tick)
 			end
 		end
 		if not Animations.CanMove(me) and victim and victim.alive and GetDistance2D(me,victim) <= 2000 then
-			if tick > sleep[1] then
+			if tick > castsleep then
 				if not Animations.isAttacking(me) then
-					local Q = me:GetAbility(1) local W = me:GetAbility(2)
-					local medallion = me:FindItem("item_medallion_of_courage")
-					local abyssal = me:FindItem("item_abyssal_blade")
-					local butterfly = me:FindItem("item_butterfly")
-					local mom = me:FindItem("item_mask_of_madness")
-					local satanic = me:FindItem("item_satanic")
-					local BlackKingBar = me:FindItem("item_black_king_bar")
-					local distance = GetDistance2D(victim,me)
-					local disable = victim:IsSilenced() or victim:IsHexed() or victim:IsStunned() or victim:IsLinkensProtected()
+					local Q, W = me:GetAbility(1), me:GetAbility(2)
+					local medallion, mom, blackkingbar = me:FindItem("item_medallion_of_courage"), me:FindItem("item_mask_of_madness"), me:FindItem("item_black_king_bar")
+					local abyssal, butterfly, satanic = me:FindItem("item_abyssal_blade"), me:FindItem("item_butterfly"), me:FindItem("item_satanic")
+					local distance, disabled = GetDistance2D(victim,me), victim:IsSilenced() or victim:IsHexed() or victim:IsStunned() or victim:IsLinkensProtected()
 					if Q and Q:CanBeCasted() and GetDistance2D(victim,me) <= Q.castRange then
 						table.insert(castQueue,{math.ceil(Q:FindCastPoint()*1000),Q,victim,true})
 					end
 					if W and W:CanBeCasted() and me:CanCast() then
 						table.insert(castQueue,{math.ceil(W:FindCastPoint()*1000),W,victim})
 					end
-					if medallion and medallion:CanBeCasted() and distance <= attackRange+100 then
+					if medallion and medallion:CanBeCasted() and distance <= me.attackRange+100 then
 						table.insert(castQueue,{math.ceil(medallion:FindCastPoint()*1000),medallion,victim})
 					end
-					if abyssal and abyssal:CanBeCasted() and distance <= abyssal.castRange and not disable then
+					if abyssal and abyssal:CanBeCasted() and distance <= abyssal.castRange and not disabled then
 						table.insert(castQueue,{math.ceil(abyssal:FindCastPoint()*1000),abyssal,victim})
 					end
 					if butterfly and butterfly:CanBeCasted() and me:CanCast() then
 						table.insert(castQueue,{100,butterfly})
 					end
-					if mom and mom:CanBeCasted() and distance <= attackRange+100 then
+					if mom and mom:CanBeCasted() and distance <= me.attackRange+100 then
 						table.insert(castQueue,{100,mom})
 					end
-					if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+100 then
+					if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= me.attackRange+100 then
 						table.insert(castQueue,{100,satanic})
 					end
-					if BlackKingBar and BlackKingBar:CanBeCasted() and me:CanCast() then
+					if ScriptConfig.bkb and blackkingbar and blackkingbar:CanBeCasted() and me:CanCast() then
 						local heroes = entityList:GetEntities(function (v) return v.type==LuaEntity.TYPE_HERO and v.alive and v.visible and v.team~=me.team and me:GetDistance2D(v) <= 1200 end)
 						if #heroes == 3 then
-							table.insert(castQueue,{100,BlackKingBar})
+							table.insert(castQueue,{100,blackkingbar})
 						elseif #heroes == 4 then
-							table.insert(castQueue,{100,BlackKingBar})
+							table.insert(castQueue,{100,blackkingbar})
 						elseif #heroes == 5 then
-							table.insert(castQueue,{100,BlackKingBar})
+							table.insert(castQueue,{100,blackkingbar})
 							return
 						end
 					end
 				end
 				me:Attack(victim)
-				sleep[1] = tick + 100
+				castsleep = tick + 150
 			end
-		elseif tick > sleep[2] then
+		elseif tick > move then
 			if victim then
 				if victim.visible then
 					local xyz = SkillShot.PredictedXYZ(victim,me:GetTurnTime(victim)*1000+client.latency+500)
@@ -128,7 +112,7 @@ function Main(tick)
 					me:Follow(victim)
 				end
 			end
-			sleep[2] = tick + 100
+			move = tick + 150
 			start = false
 		end
 	elseif victim then
@@ -147,28 +131,18 @@ function Load()
 		if me.classId ~= CDOTA_Unit_Hero_PhantomAssassin then 
 			script:Disable() 
 		else
-			play = true
-			victim = nil
-			start = false
-			resettime = nil
-			myhero = me.classId
-			rec[1].w = 90*rate + 30*0*rate + 65*rate rec[1].visible = true
-			rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate rec[2].visible = true
-			rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate rec[3].visible = true
+			ScriptConfig:SetVisible(true)
+			play, victim, start, resettime, myhero = true, nil, false, nil, me.classId
 			script:RegisterEvent(EVENT_FRAME, Main)
 			script:UnregisterEvent(Load)
 		end
-	end
+	end	
 end
 
 function Close()
-	myhero = nil
-	victim = nil
-	start = false
-	resettime = nil
-	rec[1].visible = false
-	rec[2].visible = false
-	rec[3].visible = false
+	myhero, victim, start, resettime = nil, nil, false, nil
+	ScriptConfig:SetVisible(false)
+	collectgarbage("collect")
 	if play then
 		script:UnregisterEvent(Main)
 		script:RegisterEvent(EVENT_TICK,Load)
--- Scripts/slark.lua
+++ Scripts/slark.lua
@@ -1,34 +1,24 @@
---<<Download texture https://mega.co.nz/#!8AZiFAbY!kMdEz0Fezz6cRzpVPrsNJTuUd4RFwHqDSI3cOV51U34 and unpack to nyanui/other>>
-
-require("libs.ScriptConfig")
+require("libs.HotkeyConfig2")
 require("libs.Utils")
 require("libs.TargetFind")
 require("libs.Animations")
 require("libs.Skillshot")
 
-local config = ScriptConfig.new()
-config:SetParameter("Hotkey", "32", config.TYPE_HOTKEY)
-config:SetParameter("distanceXYZ", 400)
-config:Load()
+ScriptConfig = ConfigGUI:New(script.name)
+script:RegisterEvent(EVENT_KEY, ScriptConfig.Key, ScriptConfig)
+script:RegisterEvent(EVENT_TICK, ScriptConfig.Refresh, ScriptConfig)
+ScriptConfig:SetName("Slark")
+ScriptConfig:SetExtention(-.3)
+ScriptConfig:SetVisible(false)
 
-local play = false local myhero = nil local victim = nil local start = false local resettime = nil local sleep = {0,0,0}
-local rate = client.screenSize.x/1600 local rec = {} local castQueue = {}
-rec[1] = drawMgr:CreateRect(70*rate,26*rate,270*rate,60*rate,0xFFFFFF30,drawMgr:GetTextureId("NyanUI/other/CM_status_1")) rec[1].visible = false
-rec[2] = drawMgr:CreateText(175*rate,52*rate,0xFFFFFF90,"Target :",drawMgr:CreateFont("manabarsFont","Arial",18*rate,700)) rec[2].visible = false
-rec[3] = drawMgr:CreateRect(220*rate,54*rate,16*rate,16*rate,0xFFFFFF30) rec[3].visible = false
+ScriptConfig:AddParam("hotkey","Key",SGC_TYPE_ONKEYDOWN,false,false,32)
+
+play, myhero, victim, start, resettime, castQueue, castsleep, move = false, nil, nil, false, false, {}, 0, 0
 
 function Main(tick)
 	if not PlayingGame() then return end
 	local me = entityList:GetMyHero()
 	local ID = me.classId if ID ~= myhero then return end
-
-	if victim and victim.visible then
-		if not rec[i] then
-			rec[3].textureId = drawMgr:GetTextureId("NyanUI/miniheroes/"..victim.name:gsub("npc_dota_hero_",""))
-		end
-	else
-		rec[3].textureId = drawMgr:GetTextureId("NyanUI/spellicons/doom_bringer_empty1")
-	end
 
 	for i=1,#castQueue,1 do
 		local v = castQueue[1]
@@ -38,22 +28,20 @@ function Main(tick)
 			ability = me:FindItem(ability)
 		end
 		if ability and me:SafeCastAbility(ability,v[3],false) then
-			sleep[3] = tick + v[1] + client.latency
+			castsleep = tick + v[1] + client.latency
 			return
 		end
-	end
+	end 
 
-	local attackRange = me.attackRange	
-
-	if IsKeyDown(config.Hotkey) and not client.chat then	
-		if Animations.CanMove(me) or not start or (victim and GetDistance2D(victim,me) > attackRange+50) then
+	if ScriptConfig.hotkey then
+		if Animations.CanMove(me) or not start or (victim and GetDistance2D(victim,me) > me.attackRange	+50) then
 			start = true
 			local lowestHP = targetFind:GetLowestEHP(3000, phys)
 			if lowestHP and (not victim or GetDistance2D(me,victim) > 600 or not victim.alive or lowestHP.health < victim.health) and SleepCheck("victim") then			
 				victim = lowestHP
 				Sleep(250,"victim")
 			end
-			if victim and GetDistance2D(victim,me) > attackRange+200 and victim.visible then
+			if victim and GetDistance2D(victim,me) > me.attackRange	+200 and victim.visible then
 				local closest = targetFind:GetClosestToMouse(me,2000)
 				if closest and (not victim or closest.handle ~= victim.handle) then 
 					victim = closest
@@ -61,26 +49,21 @@ function Main(tick)
 			end
 		end
 		if not Animations.CanMove(me) and victim and GetDistance2D(me,victim) <= 2000 then
-			if tick > sleep[1] then
+			if tick > castsleep then
 				if not Animations.isAttacking(me) and victim.alive and victim.visible then
 					local immune = victim:DoesHaveModifier("modifier_omniknight_repel") or victim:DoesHaveModifier("modifier_black_king_bar_immune")
 					local disable = victim:IsHexed() or victim:IsStunned() or victim:IsLinkensProtected()
 					local shadowplay = me:DoesHaveModifier("modifier_item_silver_edge_windwalk") or me:DoesHaveModifier("modifier_item_invisibility_edge_windwalk")
-					local abyssal = me:FindItem("item_abyssal_blade")
-					local butterfly = me:FindItem("item_butterfly")
-					local mom = me:FindItem("item_mask_of_madness")
-					local satanic = me:FindItem("item_satanic")
-					local BlackKingBar = me:FindItem("item_black_king_bar")
-					local Q = me:GetAbility(1)
-					local W = me:GetAbility(2)
-					local R = me:GetAbility(4) 
-					local distance = GetDistance2D(victim,me)
+					local abyssal, butterfly, mom, satanic, bkb = me:FindItem("item_abyssal_blade"), me:FindItem("item_butterfly"), me:FindItem("item_mask_of_madness"), me:FindItem("item_satanic"), me:FindItem("item_black_king_bar")
+					local Q, W, R, distance = me:GetAbility(1), me:GetAbility(2), me:GetAbility(4), GetDistance2D(victim,me)
 					if not shadowplay then
-						if Q and Q:CanBeCasted() and me:CanCast() and not immune and distance <= 325 then
-							table.insert(castQueue,{100,Q})
-						end
-						if W and W:CanBeCasted() and me:CanCast() and not immune and distance <= config.distanceXYZ then
-							table.insert(castQueue,{1000+math.ceil(W:FindCastPoint()*1000),W})
+						if not immune then
+							if Q and Q:CanBeCasted() and me:CanCast() and distance <= 325 then
+								table.insert(castQueue,{100,Q})
+							end
+							if W and W:CanBeCasted() and me:CanCast() and distance <= 580 and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, victim))) - 0.10, 0)) == 0 then
+								table.insert(castQueue,{1000+math.ceil(W:FindCastPoint()*1000),W})
+							end
 						end
 						if me.health < me.maxHealth*0.25 and R and R:CanBeCasted() then
 							table.insert(castQueue,{1000+math.ceil(R:FindCastPoint()*1000),R})
@@ -91,42 +74,42 @@ function Main(tick)
 						if butterfly and butterfly:CanBeCasted() and me:CanCast() then
 							table.insert(castQueue,{100,butterfly})
 						end
-						if mom and mom:CanBeCasted() and distance <= attackRange+100 then
+						if mom and mom:CanBeCasted() and distance <= me.attackRange	+100 then
 							table.insert(castQueue,{100,mom})
 						end
-						if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= attackRange+100 then
+						if satanic and satanic:CanBeCasted() and me.health/me.maxHealth <= 0.4 and distance <= me.attackRange	+100 then
 							table.insert(castQueue,{100,satanic})
 						end
-						if BlackKingBar and BlackKingBar:CanBeCasted() and me:CanCast() then
+						if bkb and bkb:CanBeCasted() and me:CanCast() then
 							local heroes = entityList:GetEntities(function (v) return v.type==LuaEntity.TYPE_HERO and v.alive and v.visible and v.team~=me.team and me:GetDistance2D(v) <= 1200 end)
 							if #heroes == 3 then
-								table.insert(castQueue,{100,BlackKingBar})
+								table.insert(castQueue,{100,bkb})
 							elseif #heroes == 4 then
-								table.insert(castQueue,{100,BlackKingBar})
+								table.insert(castQueue,{100,bkb})
 							elseif #heroes == 5 then
-								table.insert(castQueue,{100,BlackKingBar})
+								table.insert(castQueue,{100,bkb})
 								return
 							end
 						end
 					end
 				end
-				if GetDistance2D(victim,me) > me.attackRange+50 then
-					local xyz = SkillShot.PredictedXYZ(victim,me:GetTurnTime(victim)*1000+300+ client.latency)
+				me:Attack(victim)
+				castsleep = tick + 200
+			end
+		elseif tick > move then
+			if victim then
+				if victim.visible then
+					local xyz = SkillShot.PredictedXYZ(victim,me:GetTurnTime(victim)*1000+client.latency+500)
 					me:Move(xyz)
 				else
-					me:Attack(victim)
+					me:Follow(victim)
 				end
-				sleep[1] = tick + 100
 			end
-		elseif tick > sleep[2] then 
-			if not Animations.isAttacking(me) then
-				me:Move(client.mousePosition)
-			end
-			sleep[2] = tick + 100
+			move = tick + 200
 			start = false
 		end
 	elseif victim then
-			if not resettime then
+		if not resettime then
 			resettime = client.gameTime
 		elseif (client.gameTime - resettime) >= 6 then
 			victim = nil		
@@ -141,14 +124,8 @@ function Load()
 		if me.classId ~= CDOTA_Unit_Hero_Slark then 
 			script:Disable() 
 		else
-			play = true
-			victim = nil
-			start = false
-			resettime = nil
-			myhero = me.classId
-			rec[1].w = 90*rate + 30*0*rate + 65*rate rec[1].visible = true
-			rec[2].x = 30*rate + 90*rate + 30*0*rate + 65*rate - 95*rate rec[2].visible = true
-			rec[3].x = 80*rate + 90*rate + 30*0*rate + 65*rate - 50*rate rec[3].visible = true
+			ScriptConfig:SetVisible(true)
+			play, victim, start, resettime, myhero = true, nil, false, nil, me.classId
 			script:RegisterEvent(EVENT_FRAME, Main)
 			script:UnregisterEvent(Load)
 		end
@@ -156,13 +133,9 @@ function Load()
 end
 
 function Close()
-	myhero = nil
-	victim = nil
-	start = false
-	resettime = nil
-	rec[1].visible = false
-	rec[2].visible = false
-	rec[3].visible = false
+	myhero, victim, start, resettime = nil, nil, false, nil
+	ScriptConfig:SetVisible(false)
+	collectgarbage("collect")
 	if play then
 		script:UnregisterEvent(Main)
 		script:RegisterEvent(EVENT_TICK,Load)
