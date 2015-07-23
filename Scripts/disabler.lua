c0d892376ad6cef8f3772809b583d8165cd13566
K L A U S N O M I <theklausnomi@gmail.com>
2015-07-24 05:33:27 +0800

    12dfsdgsdfggfdg

--- Scripts/disabler.lua
+++ Scripts/disabler.lua
@@ -1,22 +1,73 @@
---<<Disabler of blinkers and initiators>>
---===By Blaxpirit===--
-
 require("libs.Utils")
 require("libs.ScriptConfig")
-require("libs.Initiators")
-require("libs.DisableSpells")
 
-local config = ScriptConfig.new()
+config = ScriptConfig.new()
 config:SetParameter("Active", "113", config.TYPE_HOTKEY)
 config:SetParameter("indent", 255)
 config:Load()
 
-local active = false local play = false local activated = 0 local hero = {} local icon = {} local sleepTick = nil
+monitor = client.screenSize.x/1600
+statusText = drawMgr:CreateText(290*monitor,42*monitor,0xFF3399FF,"Disabler: Blink",drawMgr:CreateFont("text","Tahoma",11*monitor,550*monitor)) statusText.visible = false
 
-local monitor = client.screenSize.x/1600
-local F11 = drawMgr:CreateFont("F11","Tahoma",11*monitor,550*monitor) 
-local statusText = drawMgr:CreateText(290*monitor,42*monitor,0xFF3399FF,"Disabler: Blink",F11) statusText.visible = false
- 
+active, play, activated, hero, icon, sleepTick = false, false, 0, {}, {}, nil
+
+DisableSpell = {
+	npc_dota_hero_crystal_maiden = {Spell = "crystal_maiden_frostbite"},
+	npc_dota_hero_drow_ranger = {Spell = "drow_ranger_silence"},
+	npc_dota_hero_morphling = {Spell = "morphling_adaptive_strike"},
+	npc_dota_hero_sandking = {Spell = "sandking_burrowstrike"},
+	npc_dota_hero_storm_spirit = {Spell = "storm_spirit_electric_vortex"},
+	npc_dota_hero_sven = {Spell = "sven_storm_bolt"},
+	npc_dota_hero_tiny = {Spell = "tiny_avalanche"},
+	npc_dota_hero_vengefulspirit = {Spell = "vengefulspirit_magic_missile"},
+	npc_dota_hero_riki = {Spell = "riki_smoke_screen"},
+	npc_dota_hero_enigma = {Spell = "enigma_malefice"},
+	npc_dota_hero_death_prophet = {Spell = "death_prophet_silence"},
+	npc_dota_hero_night_stalker = {Spell = "night_stalker_crippling_fear"},
+	npc_dota_hero_doom_bringer = {Spell = "doom_bringer_doom"},
+	npc_dota_hero_oracle = {Spell = "oracle_fates_edict"},
+	npc_dota_hero_zuus = {Spell = "zuus_lightning_bolt"},
+	--[[npc_dota_hero_chaos_knight = {Spell = "chaos_knight_chaos_bolt"},]]
+	--[[npc_dota_hero_ogre_magi = {Spell = "ogre_magi_fireblast"},]]
+	--[[npc_dota_hero_tusk = {Spell = "death_prophet_silence"},]]
+	--[[npc_dota_hero_batrider = {Spell = "batrider_flaming_lasso"},]]
+	--[[npc_dota_hero_skeleton_king = {Spell = "death_prophet_silence"},]]
+}
+
+Initiation = {
+	npc_dota_hero_faceless_void = {Spell = "faceless_void_time_walk"},
+	npc_dota_hero_shredder = {Spell = "shredder_timber_chain"},
+	npc_dota_hero_phoenix = {Spell = "phoenix_icarus_dive"},
+	npc_dota_hero_antimage = {Spell = "antimage_blink"},
+	npc_dota_hero_legion_commander = {Spell = "legion_commander_duel"},
+	npc_dota_hero_mirana = {Spell = "mirana_leap"},
+	npc_dota_hero_phantom_lancer = {Spell = "phantom_lancer_doppelwalk"},
+	npc_dota_hero_terrorblade = {Spell = "terrorblade_sunder"},
+	npc_dota_hero_huskar = {Spell = "huskar_life_break"},
+	npc_dota_hero_rattletrap = {Spell = "rattletrap_hookshot"},
+	npc_dota_hero_earth_spirit = {Spell = "earth_spirit_rolling_boulder"},
+	npc_dota_hero_chaos_knight = {Spell = "chaos_knight_reality_rift"},
+	npc_dota_hero_morphling = {Spell = "morphling_waveform"},
+	npc_dota_hero_vengefulspirit = {Spell = "vengefulspirit_nether_swap"},
+	npc_dota_hero_phantom_assassin = {Spell = "phantom_assassin_phantom_strike"},
+	npc_dota_hero_riki = {Spell = "riki_blink_strike"},
+	npc_dota_hero_weaver = {Spell = "weaver_time_lapse"},
+	npc_dota_hero_sandking = {Spell = "sandking_epicenter"},
+	npc_dota_hero_slark = {Spell = "slark_pounce"},
+	npc_dota_hero_crystal_maiden = {Spell = "crystal_maiden_freezing_field"},
+	npc_dota_hero_pudge = {Spell = "pudge_dismember"},
+	npc_dota_hero_Bane = {Spell = "bane_fiends_grip"},
+	npc_dota_hero_Enigma = {Spell = "enigma_black_hole"},
+	npc_dota_hero_witch_doctor = {Spell = "witch_doctor_death_ward"},
+	npc_dota_hero_queenofpain = {Spell = "queenofpain_blink"},
+	npc_dota_hero_storm_spirit = {Spell = "storm_spirit_ball_lightning"},
+	npc_dota_hero_puck = {Spell = "puck_illusory_orb"},
+	npc_dota_hero_magnataur = {Spell = "magnataur_skewer"},
+	npc_dota_hero_ember_spirit = {Spell = "ember_spirit_fire_remnant"},
+	--[[npc_dota_hero_furion = {Spell = "furion_teleportation"},]]
+	--[[npc_dota_hero_pugna = {Spell = "pugna_life_drain"},]]
+}
+
 function Key(msg,code)
 	if IsKeyDown(config.Active) and not client.chat then
 		active = not active
@@ -48,7 +99,7 @@ function Tick( tick )
 	if not PlayingGame() or sleepTick and sleepTick > tick then return end
 	me = entityList:GetMyHero()
 	
-	local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,team=me:GetEnemyTeam(),illusion=false})
+	local enemies = entityList:GetEntities({type = LuaEntity.TYPE_HERO, alive = true, team = me:GetEnemyTeam(), illusion = false})
 	for i,v in ipairs(enemies) do
 		target = v
 		local IV  = v:IsInvul()
@@ -102,6 +153,7 @@ function Tick( tick )
 					UseAstral()
 					UseHalberdtarget()
 					UseEtherealtarget()
+					UseSearingChains()
 				elseif active then
 					UseHex()
 					UseSheepStickTarget()
@@ -115,6 +167,7 @@ function Tick( tick )
 					UseEulScepterTarget()
 					UseAstral()
 					UseHalberdtarget()
+					UseSearingChains()
 				elseif Initiation[v.name] then
 					local Spell = v:FindSpell(Initiation[v.name].Spell)
 					if Spell and Spell.level ~= 0 and Spell.cd > Spell:GetCooldown(Spell.level) - 1.6 then
@@ -131,6 +184,7 @@ function Tick( tick )
 						UseAstral()
 						UseHalberdtarget()
 						UseEtherealtarget()
+						UseSearingChains()
 					end
 				end
 			end
@@ -391,6 +445,20 @@ function UsePucksRift()
 	end
 end
 
+function UseSearingChains()
+	if activated == 0 then
+		local disable = me:FindSpell("ember_spirit_searing_chains")
+		if disable and disable:CanBeCasted() and me:CanCast() then
+			if target and GetDistance2D(me,target) < 400 then
+				me:SafeCastAbility(disable)
+				activated = 1
+				sleepTick = GetTick() + 100
+				return
+			end
+		end
+	end
+end
+
 function UseHeroSpell()
 	if activated == 0 then
 		if DisableSpell[me.name] then
@@ -418,11 +486,9 @@ function Load()
 end
 
 function Close()
-	statusText.visible = false
-	hero = {}
-	icon = {}
-	collectgarbage("collect")
 	if play then
+		statusText.visible, hero, icon = false, {}, {}
+		collectgarbage("collect")
 		script:UnregisterEvent(Tick)
 		script:UnregisterEvent(Key)
 		script:RegisterEvent(EVENT_TICK,Load)
