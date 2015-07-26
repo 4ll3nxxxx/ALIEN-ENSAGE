e4796ef7cb979fda911e85208d22115df911a480
K L A U S N O M I <theklausnomi@gmail.com>
2015-07-27 00:14:04 +0800

    forcestaff

--- Scripts/auto_Items.lua
+++ Scripts/auto_Items.lua
@@ -35,7 +35,7 @@ function Tick(tick)
 					for i,t in ipairs(entityList:GetEntities(function (k) return (k.classId == CDOTA_BaseNPC_Tower or k.classId == CDOTA_BaseNPC_Building) and k.alive and k.team == me.team end)) do
 						if entityList:GetMyPlayer().orderId == Player.ORDER_ATTACKENTITY and entityList:GetMyPlayer().target and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, v))) - 0.30, 0)) == 0 and GetDistance2D(v,me) >= forcestaff.castRange-200 then
 							me:CastAbility(forcestaff,me) break
-						elseif entityList:GetMyPlayer().orderId == Player.ORDER_MOVETOPOSITION and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, t))) - 0.10, 0)) == 0 and (math.max(math.abs(FindAngleR(v) - math.rad(FindAngleBetween(v, me))) - 0.20, 0)) < 0.15 then
+						elseif entityList:GetMyPlayer().orderId == Player.ORDER_MOVETOPOSITION and not Animations.isAttacking(me) and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, t))) - 0.10, 0)) == 0 and (math.max(math.abs(FindAngleR(v) - math.rad(FindAngleBetween(v, me))) - 0.20, 0)) < 0.15 then
 							if (Animations.isAttacking(v) or z.abilityPhase and GetDistance2D(v,me) <= z.castRange+300) then
 								me:CastAbility(forcestaff,me) break
 							end
