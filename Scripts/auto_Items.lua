055f92bf9e9f1623a91fbfe162dc4f7ec655b177
K L A U S N O M I <theklausnomi@gmail.com>
2015-07-26 21:59:34 +0800

    forcestaff

--- Scripts/auto_Items.lua
+++ Scripts/auto_Items.lua
@@ -12,18 +12,32 @@ play, spellList = false, {
 function Tick(tick)
     if not PlayingGame() or not SleepCheck() then return end Sleep(150+client.latency)
 	local me = entityList:GetMyHero()
+	local hero = entityList:GetEntities(function (v) return v.hero and v.alive and v.visible and v.team == me:GetEnemyTeam() end)
 	local bloodstone, glimmercape = me:FindItem("item_bloodstone"), me:FindItem("item_glimmer_cape")
 	local bottle, stick = me:FindItem("item_bottle"), me:FindItem("item_magic_stick") or me:FindItem("item_magic_wand")
-	local phaseboots = me:FindItem("item_phase_boots")
+	local phaseboots, forcestaff = me:FindItem("item_phase_boots"), me:FindItem("item_force_staff")
 	local midas = me:FindItem("item_hand_of_midas")
 	if not me:IsInvisible() and not me:IsChanneling() and me.alive then
 		if bloodstone and bloodstone:CanBeCasted() then
-			for i,v in ipairs(entityList:GetEntities({type = LuaEntity.TYPE_HERO, alive = true, visible = true, team = me:GetEnemyTeam()})) do						
+			for i,v in ipairs(hero) do						
 				for i,z in ipairs(v.abilities) do
 					if GetDistance2D(v,me) <= z.castRange+100 and (math.max(math.abs(FindAngleR(v) - math.rad(FindAngleBetween(v, me))) - 0.20, 0)) < 0.15 then
 						local dmg, dmg2 = me:DamageTaken(AbilityDamage.GetDamage(z, me.healthRegen), AbilityDamage.GetDmgType(z), v), me:DamageTaken(v.dmgMin + v.dmgBonus, DAMAGE_PHYS, v)
 						if (z.abilityPhase and me.health < dmg2 or me.health < dmg) then
 							me:CastAbility(bloodstone,me.position)
+						end
+					end
+				end
+			end
+		elseif forcestaff and forcestaff:CanBeCasted() then
+			for i,v in ipairs(hero) do
+				for i,z in ipairs(v.abilities) do
+					local fountain = entityList:GetEntities({classId = CDOTA_Unit_Fountain,team = me.team})[1]
+					if entityList:GetMyPlayer().orderId == Player.ORDER_ATTACKENTITY and entityList:GetMyPlayer().target and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, v))) - 0.30, 0)) == 0 and GetDistance2D(v,me) >= forcestaff.castRange then
+						me:CastAbility(forcestaff,me)
+					elseif (entityList:GetMyPlayer().orderId == Player.ORDER_MOVETOPOSITION and (math.max(math.abs(FindAngleR(me) - math.rad(FindAngleBetween(me, fountain))) - 0.40, 0)) == 0 and (math.max(math.abs(FindAngleR(v) - math.rad(FindAngleBetween(v, me))) - 0.20, 0)) == 0) then
+						if Animations.isAttacking(v) or z.abilityPhase and GetDistance2D(v,me) <= z.castRange+100 then
+							me:CastAbility(forcestaff,me)
 						end
 					end
 				end
@@ -36,7 +50,7 @@ function Tick(tick)
 				end
 			end
 		elseif midas and midas:CanBeCasted() then
-			for _,v in ipairs(entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_CREEP and v.team ~= me.team and v.alive and v.visible and v.spawned and v.level >= 5 and not v.ancient and v.health > 0 and v.attackRange < 650 and v:GetDistance2D(me) < midas.castRange + 25 end)) do
+			for i,v in ipairs(entityList:GetEntities(function (v) return v.type == LuaEntity.TYPE_CREEP and v.team ~= me.team and v.alive and v.visible and v.spawned and v.level >= 5 and not v.ancient and v.health > 0 and v.attackRange < 650 and v:GetDistance2D(me) < midas.castRange + 25 end)) do
 				me:CastAbility(midas,v)
 			end
 		elseif bottle and bottle:CanBeCasted() and me:DoesHaveModifier("modifier_fountain_aura_buff") and not me:DoesHaveModifier("modifier_bottle_regeneration") and (me.health < me.maxHealth or me.mana < me.maxMana) then
