ff77fe33e5346af1f1d775aa68e85693beb1bcba
K L A U S N O M I <theklausnomi@gmail.com>
2015-07-29 01:20:33 +0800

    faster

--- Scripts/beta.lua
+++ Scripts/beta.lua
@@ -2,41 +2,39 @@ require("libs.Utils")
 require("libs.TargetFind")
 require("libs.Skillshot")
 
-play = false
-
 function Tick(tick)
-    if not PlayingGame() or not SleepCheck() then return end
+    if not PlayingGame() then return end
     local me, target = entityList:GetMyHero(), targetFind:GetClosestToMouse(100)
     if IsKeyDown(32) and target and target.alive then		
 		for i,k in ipairs(me.abilities) do
-			if k:CanBeCasted() and k:CanBeCasted() and me:CanCast() then
+			if k:CanBeCasted() and k:CanBeCasted() and me:CanCast() and SleepCheck("abilities") then
 				if k:IsBehaviourType(LuaEntityAbility.BEHAVIOR_UNIT_TARGET) and GetDistance2D(target,me) <= k.castRange-50 then
-					me:CastAbility(k,target) Sleep(k:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+					me:CastAbility(k,target) Sleep(100,"abilities")
 				end
 				if k:IsBehaviourType(LuaEntityAbility.BEHAVIOR_NO_TARGET) and GetDistance2D(target,me) <= me.attackRange+100 then
-					me:CastAbility(k) Sleep(k:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+					me:CastAbility(k) Sleep(100,"abilities")
 				end
 				if k:IsBehaviourType(LuaEntityAbility.BEHAVIOR_POINT) then
 					local xyz = SkillShot.SkillShotXYZ(me,target,k:FindCastPoint()*1000+client.latency+me:GetTurnTime(target)*1000,1000)
 					if xyz and GetDistance2D(target,me) <= k.castRange-50 then
-						me:CastAbility(k,xyz) Sleep(k:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+						me:CastAbility(k,xyz) Sleep(100,"abilities")
 					end
 				end
 			end
 		end
 		for i,j in ipairs(me.items) do
-			if j:CanBeCasted() and j:CanBeCasted() and me:CanCast() and j.name ~= "item_travel_boots" and j.name ~= "item_travel_boots_2" and j.name ~= "item_tpscroll" then
+			if j:CanBeCasted() and j:CanBeCasted() and me:CanCast() and j.name ~= "item_travel_boots" and j.name ~= "item_travel_boots_2" and j.name ~= "item_tpscroll" and SleepCheck("items") then
 				if j:IsBehaviourType(LuaEntityAbility.BEHAVIOR_UNIT_TARGET) and GetDistance2D(target,me) <= j.castRange-50 then
-					me:CastAbility(j,target) Sleep(j:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+					me:CastAbility(j,target) Sleep(100,"items")
 				end
 				if j:IsBehaviourType(LuaEntityAbility.BEHAVIOR_NO_TARGET) then
-					me:CastAbility(j) Sleep(j:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+					me:CastAbility(j) Sleep(100,"items")
 				end
 				if j:IsBehaviourType(LuaEntityAbility.BEHAVIOR_POINT) then
 					if j.name ~= "item_blink" and GetDistance2D(target,me) <= j.castRange-50  then
-						me:CastAbility(j,target.position) Sleep(j:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+						me:CastAbility(j,target.position) Sleep(100,"items")
 					elseif j.name == "item_blink" and GetDistance2D(target,me) <= 1199 and GetDistance2D(target,me) >= me.attackRange+450 then
-						me:CastAbility(j,target.position) Sleep(j:FindCastPoint()*1000+me:GetTurnTime(target)*1000)
+						me:CastAbility(j,target.position) Sleep(100,"items")
 					end
 				end
 			end
